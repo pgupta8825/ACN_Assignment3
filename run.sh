df5bdc3e4c5a50e2ef1f7ad95aa9aa19086065a5
pgupta8825 <57871807+pgupta8825@users.noreply.github.com>
2023-10-28 15:45:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 sudo mn --custom custom_topology.py --topo mytopo
 
 # You can also add additional commands to run inside Mininet, for example:
-# sudo mn --custom custom_topology.py --topo mytopo --command "python /path/to/myscript.py"
+# sudo mn --custom Create_topology.py --topo mytopo --command "python /path/to/myscript.py"
 
 # Additional commands can be added as needed
